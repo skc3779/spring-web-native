6875326fedc7a6e5f07a989a24665b0e38766b14
kangchun.seo <skc3779@gmail.com>
2023-07-30 17:06:32 +0900

    spring web native 샘플코드

---
+++ src/main/java/com/example/springnativeweb/SpringNativeWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnativeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeWebApplication.class, args);
+    }
+
+}
